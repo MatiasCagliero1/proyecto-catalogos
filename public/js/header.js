8e279c2648710e3fe1580d4ae76466abc9bf0919
agosCervio <acervio@udesa.edu.ar>
2021-04-14 13:19:06 -0300

    js del front end para que cambie header

--- public/js/header.js
+++ public/js/header.js
@@ -1,18 +1,19 @@
 window.addEventListener("load", function() {
 
-    let usuario = document.querySelector("#username")
-    if (usuario != null) {
+    let usuario = sessionStorage.getItem("usuario")
+    if (usuario == "hola") {
         let logueado1 = document.querySelector(".logueado1")
         logueado1.style.display = "block"
         let logueado2 = document.querySelector(".logueado2")
         logueado2.style.display = "block"
         let logueado3 = document.querySelector(".logueado3")
         logueado3.style.display = "block"
+        let logueado4 = document.querySelector(".logueado4")
+        logueado4.style.display = "block"
         let deslogueado1 = document.querySelector(".deslogueado1")
         deslogueado1.style.display = "none"
         let deslogueado2 = document.querySelector(".deslogueado2")
         deslogueado2.style.display = "none"
-
     }
 
 
