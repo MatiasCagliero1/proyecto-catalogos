dd1a1bca4360a347a3a476b1c829b2e4e70e59f5
agosCervio <acervio@udesa.edu.ar>
2021-04-14 01:04:12 -0300

    para que cambie el header si se logueo

---
+++ public/js/header.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function() {
+
+    let usuario = document.querySelector("#username")
+    if (usuario != null) {
+        let logueado1 = document.querySelector(".logueado1")
+        logueado1.style.display = "block"
+        let logueado2 = document.querySelector(".logueado2")
+        logueado2.style.display = "block"
+        let logueado3 = document.querySelector(".logueado3")
+        logueado3.style.display = "block"
+        let deslogueado1 = document.querySelector(".deslogueado1")
+        deslogueado1.style.display = "none"
+        let deslogueado2 = document.querySelector(".deslogueado2")
+        deslogueado2.style.display = "none"
+
+    }
+
+
+
+
+
+
+})
---
+++ public/js/logIn.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function() {
+
+
+    let logIn = true
+    document.querySelector(".botonregistrarse").addEventListener("click", function() {
+        logIn = false
+        alert(logIn)
+        if (logIn == false) {
+
+            let logueado1 = document.querySelector(".logueado1")
+            logueado1.style.display = "block"
+            let logueado2 = document.querySelector(".logueado2")
+            logueado2.style.display = "block"
+            let logueado3 = document.querySelector(".logueado3")
+            logueado3.style.display = "block"
+            let logueado4 = document.querySelector(".logueado4")
+            logueado4.style.display = "block"
+            let deslogueado1 = document.querySelector(".deslogueado1")
+            deslogueado1.style.display = "none"
+            let deslogueado2 = document.querySelector(".deslogueado2")
+            deslogueado2.style.display = "none"
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+})
