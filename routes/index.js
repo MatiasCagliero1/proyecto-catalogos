c406e150310d99597a99504b99e2b1f9afa72e99
agosCervio <acervio@udesa.edu.ar>
2021-04-14 01:04:28 -0300

    ruta

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//router.get('/', function(req, res, next) {
+//res.render('index', { title: 'Express' });
+//});
 
 // Requiriendo controlador 
 const mainController = require('../controllers/mainController')
 
 // Ruta pagina index
-router.get('/home', mainController.index)
+router.get('/', mainController.index)
 
 
 module.exports = router;
