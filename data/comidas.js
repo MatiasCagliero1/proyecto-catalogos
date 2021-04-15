9f4936c22a9d2ec006ec0624f22ba57bc533501a
agosCervio <acervio@udesa.edu.ar>
2021-04-15 16:29:03 -0300

    comidas data

---
+++ data/comidas.js
@@ -0,0 +1,37 @@
+let comidas = {
+    listas: [
+
+        {
+            nombre: "Coca Cola",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente"
+        }, {
+            nombre: "Coca Cola",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente"
+        }, {
+            nombre: "Coca Cola",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente"
+        }, {
+            nombre: "Coca Cola",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente"
+        }, {
+            nombre: "Coca Cola",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente"
+        }, {
+            nombre: "Coca Cola",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente"
+        }
+
+    ]
+}
