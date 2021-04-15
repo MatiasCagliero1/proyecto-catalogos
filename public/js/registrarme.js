127849ba0714d259ac11eadf6bbc7bd69a808389
agosCervio <acervio@udesa.edu.ar>
2021-04-15 15:11:35 -0300

    registrarme js frontend

---
+++ public/js/registrarme.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function() {
+    document.querySelector(".botonregistrarse2").addEventListener("click", function() {
+        let registrame = "hola"
+        alert(registrame)
+        sessionStorage.setItem("usuario", registrame)
+        if (registrame == "hola") {
+            let logueado1 = document.querySelector(".logueado1")
+            logueado1.style.display = "block"
+            let logueado2 = document.querySelector(".logueado2")
+            logueado2.style.display = "block"
+            let logueado3 = document.querySelector(".logueado3")
+            logueado3.style.display = "block"
+            let logueado4 = document.querySelector(".logueado4")
+            logueado4.style.display = "block"
+            let deslogueado1 = document.querySelector(".deslogueado1")
+            deslogueado1.style.display = "none"
+            let deslogueado2 = document.querySelector(".deslogueado2")
+            deslogueado2.style.display = "none"
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
