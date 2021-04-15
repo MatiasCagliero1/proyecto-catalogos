06e08597625eff6546e2c0ee75330aa0ff3b639a
santikaragozian <ksantiago@udesa.edu.ar>
2021-04-15 20:24:42 -0300

    comidas a js corregido

--- data/comidas.js
+++ data/comidas.js
@@ -2,41 +2,41 @@
     [
 
         {
-            "nombre": "Coca Cola",
-            "detalle": "Rock",
-            "imagenSrc": "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
-            "stock": "permanente",
-            "precio": "110"
+            nombre: "Coca Cola",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente",
+            precio: "110"
         }, {
-            "nombre": "Fideos",
-            "detalle": "Rock",
-            "imagenSrc": "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
-            "stock": "permanente",
-            "precio": "80"
+            nombre: "Fideos",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente",
+            precio: "80"
         }, {
-            "nombre": "Carne",
-            "detalle": "Rock",
-            "imagenSrc": "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
-            "stock": "permanente",
-            "precio": "150"
+            nombre: "Carne",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente",
+            precio: "150"
         }, {
-            "nombre": "Arroz",
-            "detalle": "Rock",
-            "imagenSrc": "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
-            "stock": "permanente",
-            "precio": "80"
+            nombre: "Arroz",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente",
+            precio: "80"
         }, {
-            "nombre": "Dulce de Leche",
-            "detalle": "Rock",
-            "imagenSrc": "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
-            "stock": "permanente",
-            "precio": "150"
+            nombre: "Dulce de Leche",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente",
+            precio: "150"
         }, {
-            "nombre": "Mermelada",
-            "detalle": "Rock",
-            "imagenSrc": "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
-            "stock": "permanente",
-            "precio": "400"
+            nombre: "Mermelada",
+            detalle: "Rock",
+            imagenSrc: "https://www.pasosonline.com.ar/wp-content/uploads/2018/12/lactal-360-1.png",
+            stock: "permanente",
+            precio: "400"
         }
 
     ]
