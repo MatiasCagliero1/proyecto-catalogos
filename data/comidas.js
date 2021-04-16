7672f2f06d1cef41bf4d64750bcafa13b7ddbfc0
Matias C <matiascagliero1@hotmail.com>
2021-04-16 10:05:47 -0300

    Correcion Datos

--- data/comidas.js
+++ data/comidas.js
@@ -28,6 +28,12 @@ module.exports = {
             imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
             condicion: ['Vegetariano', 'Vegano'],
             sinTacc: false
-        },
+        },{
+            nombre: "Pan Artesano Bimbo 500grm.",
+            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
+            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
+            condicion: ['Vegetariano', 'Vegano'],
+            sinTacc: false
+        }
     ]
 }
