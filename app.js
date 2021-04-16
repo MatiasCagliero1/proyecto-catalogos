961818b96d9125a7ccbc568bb0203e3be5810b67
Matias C <matiascagliero1@hotmail.com>
2021-04-16 09:37:47 -0300

    Change

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var  searchrouter= require('./routes/index');
-var  detallerouter= require('./routes/index');
+var  productsRouter = require('./routes/products');
 
 var app = express();
 
@@ -23,8 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/search', searchrouter);
-app.use('/detalle', detallerouter);
+app.use('/product', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/products.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+
+// Requiriendo Search - Detalle - Product Add
+var productController = require('../controllers/productController');
+// Ruta pagina Search - Detalle - Product Add
+router.get('/search', productController.search);
+router.get('/search:id', productController.searchId);
+router.get('/detalle', productController.detalle);
+router.get('/detalle:id', productController.detalleId);
+router.get('/newproduct:id', productController.newProduct);
+
+module.exports = router;
