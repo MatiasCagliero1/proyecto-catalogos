5e21862d86b478a76fa86508eb00629d09749050
santikaragozian <ksantiago@udesa.edu.ar>
2021-04-19 17:11:03 -0300

    pagina Index terminada

--- data/comidas.js
+++ data/comidas.js
@@ -1,126 +1,118 @@
 module.exports = {
-    comidas: [
+    productos:[
 
+            {
+                id:'1',
+                nombre: "Pan Artesano Bimbo 500grm.",
+                detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
+                imagenSrc: "pan.jpg",
+                condicion: ['Vegetariano', 'Vegano'],
+                medida: "Kilo",
+                preciopor: "500",
+                
+            }, {
+                id:'2',
+                nombre: "Papas McCain",
+                detalle: "Papas McCain + finitas 1 kg",
+                imagenSrc: "papas.jpg",
+                condicion: null,
+                medida: "Kilo",
+                preciopor: "250",
+                
+            }, {
+                id:'3',
+                nombre: "Cerveza Lager Heineken Botella 1 L",
+                detalle: null,
+                imagenSrc: "cerveza.jpg",
+                condicion: ['Vegetariano', 'Vegano'],
+                medida: "Kilo",
+                preciopor: "175",
+        
+            }, {
+                id:'4',
+                nombre: "Lavavajillas Bio Act.Frutas Cif Bot 500 ml",
+                detalle: null,
+                imagenSrc: "lavavajillas.jpg",
+                condicion: ['Vegetariano', 'Vegano'],
+                medida: "Kilo",
+                preciopor: "110",
+                sinTacc: false
+            }, {
+                id:'5',
+                nombre: "Jabon Liquido ALA Para Diluir 500 ml",
+                detalle: null,
+                imagenSrc: "jabon.jpg",
+                condicion: ['Vegetariano', 'Vegano'],
+                medida: "Kilo",
+                preciopor: "335",
+                sinTacc: false
+            }, {
+                id:'6',
+                nombre: "Papas Noisette McCain Bsa 1 kg",
+                detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
+                imagenSrc: "papas1.jpg",
+                condicion: ['Vegetariano', 'Vegano'],
+                medida: "Kilo",
+                preciopor: "335",
+                sinTacc: false
+            },  {
+                id:'7',
+                nombre: "Alfajor Terrabusi Chocolate 50 Gr X 6 Uni",
+                detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
+                imagenSrc: "alfajor.jpg",
+                condicion: ['Vegetariano', 'Vegano'],
+                medida: "Kilo",
+                preciopor: "290",
+                sinTacc: false
+            },  {
+                id:'8',
+                nombre: "Yerba Mate C/Palo Chamigo Paq 500 Grm",
+                detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
+                imagenSrc: "yerba.jpg",
+                condicion: ['Vegetariano', 'Vegano'],
+                medida: "Kilo",
+                preciopor: "125",
+                sinTacc: false
+            },      
+                    
+        ],
+    ofertas: [
         {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }, {
-            nombre: "Pan Artesano Bimbo 500grm.",
-            detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
-            imagenSrc: "https://static.cotodigital3.com.ar/sitios/fotos/full/00293000/00293087.jpg?3.0.129d",
-            condicion: ['Vegetariano', 'Vegano'],
-            medida: "Kilo",
-            preciopor: "500",
-            sinTacc: false
-        }
-    ]
+        id:'9',
+        nombre: "Arroz gallo oro 500 grm",
+        detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
+        imagenSrc: "arroz.jpg",
+        condicion: ['Vegetariano', 'Vegano'],
+        medida: "Kilo",
+        preciopor: "240",
+        sinTacc: false
+    },{
+        id:'10',
+        nombre: "Dulce Leche Clasico SANCOR Pot 400 Grm",
+        detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
+        imagenSrc: "dulce_de_leche.jpg",
+        condicion: ['Vegetariano', 'Vegano'],
+        medida: "Kilo",
+        preciopor: "115",
+        sinTacc: false
+    },{
+        id:'11',
+        nombre: "Fideos Matarazzo 500 grm",
+        detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
+        imagenSrc: "fideos.jpg",
+        condicion: ['Vegetariano', 'Vegano'],
+        medida: "Kilo",
+        preciopor: "75",
+        sinTacc: false
+    },{
+        id:'12',
+        nombre: "PATAGONIA BERRIES Dulce Zarzamora 352 Gr",
+        detalle: "Contenido Neto: 500grm. Precio por Kilo: $479,98. SIN TACC: No.",
+        imagenSrc: "mermelada.jpg",
+        condicion: ['Vegetariano', 'Vegano'],
+        medida: "Kilo",
+        preciopor: "450",
+        sinTacc: false
+    },
+]
 }
