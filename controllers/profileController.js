fd8470bec03cbf2e32f8e8502d9b8c389fd3736b
santikaragozian <ksantiago@udesa.edu.ar>
2021-04-20 20:29:19 -0300

    perfil y editar perfil terminado (creo)

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,10 @@
+const productos = require("../data/comidas.js")
+
+
+
 module.exports = {
     index: (req, res) => {
-        return res.render("profile")
+        return res.render("profile", {productos})
     },
 
     edit: (req, res) => {
