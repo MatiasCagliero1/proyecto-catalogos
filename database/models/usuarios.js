4157aa280801ee1b0bdb42d2c23b303c59547067
agosCervio <acervio@udesa.edu.ar>
2021-05-13 16:04:30 -0300

    cambios

---
+++ database/models/usuarios.js
@@ -0,0 +1,50 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = Productos;
+    let cols = {
+
+        id: {
+            type: dataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+            defaultValue: true,
+            unique: true
+        },
+        productName: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        imgUrl: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        condicion: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        precioXmedida: {
+            type: DataTypes.DOUBLE,
+            allowNull: false
+        },
+        UserAdderId: {
+            type: dataTypes.BOOLEAN,
+            allowNull: false
+        },
+        createdAt: {
+            type: dataTypes.DATE
+        },
+        updatedAt: {
+            type: dataTypes.DATE
+        },
+
+    }
+    let config = {
+        //tableName: "productos",
+        //timestamps: true,
+        underscored: true,
+    }
+
+    const Producto = sequelize.define(alias, cols, config);
+    return Producto;
+};
