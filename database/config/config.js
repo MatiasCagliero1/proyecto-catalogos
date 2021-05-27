94b19166a5886a8466cacf53fef146ee5418148b
Matias C <matiascagliero1@hotmail.com>
2021-05-27 14:54:09 -0300

    Actializacion

--- database/config/config.js
+++ database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     "development": {
         "username": "root",
-        "password": null,
-        "database": "supermercado",
+        "password": "root",
+        "database": "SUPERMERCADO",
         "host": "127.0.0.1",
         "dialect": "mysql"
     },
