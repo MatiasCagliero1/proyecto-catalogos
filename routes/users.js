e599a006c987351d000f95afcef97a598b25235b
agosCervio <acervio@udesa.edu.ar>
2021-06-04 23:38:00 -0300

    administrador produc y usuarios

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,8 @@ router.post("/store", controladorUsuario.store)
 router.get("/login", controladorUsuario.logIn)
 router.post("/login", controladorUsuario.iniciar)
 router.get("/logout", controladorUsuario.logout)
+router.get("/admin/productos", controladorUsuario.adminProductos)
+router.get("/admin/usuarios", controladorUsuario.adminUsuarios)
 
 //Ruta pagina Profile - Profile-edit
 
