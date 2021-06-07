41248efac734b0eb9207c497d1003309c9560563
santikaragozian <ksantiago@udesa.edu.ar>
2021-06-07 16:36:29 -0300

    home ordenado por fecha

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,11 +2,11 @@ const db = require('../database/models')
 
 module.exports = {
     index: (req, res) => {
-        db.Producto.findAll(/*{
+        db.Producto.findAll({
             order: [
-                ['']
+                ['createdAt', 'DESC']
             ]
-        }*/)
+        })
         .then(respuesta=>{
             return res.render("pagIndex", {respuesta})
         })
