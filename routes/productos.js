cbff8c95a406bf18bcef94bf281d07602ab7150d
Matias C <matiascagliero1@hotmail.com>
2021-06-07 15:20:25 -0300

    Multer

--- routes/productos.js
+++ routes/productos.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+var storage = multer.diskStorage({
+
+  destination: (req, file, cb) => {
+
+    cb(null, 'public/img/productosFile')
+
+  },
+
+  filename: (req, file, cb) => {
+//Ver UniqueID
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+
+  }
+})
+
+var upload = multer({storage: storage});
 
 // Requiriendo Search - Detalle - Product Add
 var productController = require('../controllers/productController');
@@ -14,14 +33,26 @@ router.get('/detalle/:id', productController.detalle);
 router.get('/destroy/:id', productController.destroy);
 
 router.get('/newproduct', productController.newProduct);
-router.post('/newproduct/post', productController.newProductPost);
+router.post('/newproduct/post', upload.single('ProductoImg'), productController.newProductPost);
 
 router.get('/editproduct/:id', productController.editProduct);
-router.post('/editproduct/post', productController.editProductPost);
- 
+router.post('/editproduct/post', upload.single('ProductoImg'), productController.editProductPost);
+
+
+
 module.exports = router;
 
-   
+
 //No me carga el value en el edit product para mandarlo al post
-  //IF condicion == 0  sacar condition en search
-  //Como saco el id del producto que mando en newProductPost
+//IF condicion == 0  sacar where condition en search
+//Como saco el id del producto que mando en newProductPost
+//Multer
+//Llamar a varias bases de datos, como hago el fetch?
+
+
+
+
+
+//If el producto es del usuraio aparece editar y eliminar 
+
+//Se borro bien o mail el producto? . Hacer verificacion
