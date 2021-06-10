d87e7fb3db7378a9d8881e2357dc5d782d220b01
Matias C <matiascagliero1@hotmail.com>
2021-06-10 16:33:45 -0300

    Productos 2

--- db/supermercado.sql
+++ db/supermercado.sql
@@ -3,6 +3,51 @@ DROP DATABASE IF EXISTS SUPERMERCADO;
 CREATE DATABASE SUPERMERCADO;
  USE  SUPERMERCADO;
  
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(255) NOT NULL,
+  `img_name` varchar(255),
+  `detalle` varchar(255) NOT NULL,
+  `condicion` varchar(255) NOT NULL,
+  `userAdded` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_95407dd7-28ee-46dc-92a8-7956bc8217b5` (`userAdded`),
+  CONSTRAINT `FK_95407dd7-28ee-46dc-92a8-7956bc8217b5` FOREIGN KEY (`userAdded`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES 
+(1, 'Yerba Mate CHAMIGO 500g', 'yerba.jpg', 'Yerba Chamigo ideal para toda la familia.', 1, 1, '2021-04-25', '2021-04-25'),
+(2, 'Papas Noisettes Mc Cain 1kg', 'papas1.jpg', 'Papar ricas para disfrutar.', 4, 2, '2021-04-25', '2021-04-25'),
+(3, 'Papas Finas Mc Cain 1kg', 'papas.jpg', 'Papar ricas para disfrutar. Buenisimas!', 4, 3, '2021-04-25', '2021-04-25'),
+(4, 'Pan Bimbo Artesano Receta Original 500g', 'pan.jpg', 'Pan de los mejores arrtesanos de Bimbo.', 3, 3, '2021-04-25', '2021-04-25'),
+(5, 'Mermelada Patagonia Berries 100% Natural', 'mermelada.jpg', 'Mermelada rica sabor Berries.', 3, 4, '2021-04-25', '2021-04-25'),
+(6, 'Liquido limpiador ALA lavado perfecto 3L', 'jabon.jpg', 'Limpiador para ropa ALA.', 4, 4, '2021-04-25', '2021-04-25'),
+(7, 'Cerveza Heineken Premium Quality', 'cerveza.jpg', 'Heineken para disfrutar con amigos.', 4, 3, '2021-04-25', '2021-04-25'),
+(8, 'Coca Cola Original 1,5L', 'coca_cola.jpg', 'Coca Cola para compartir felicidad.', 4, 3, '2021-04-25', '2021-04-25'),
+(9, 'Alfajor Terrabusi Clatico 300g', 'alfajor.jpg', 'Alfabores ricos de chocolate.', 4, 3, '2021-04-25', '2021-04-25'),
+(10, 'Fideos Matarazzo 500g', 'fideos.jpg', 'Fideos ricos para compartir en familia. Matarazzo.', 4, 2, '2021-04-25', '2021-04-25');
+
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+--
+
+
 
 -- Table structure for table `usuarios`
 --
@@ -31,51 +76,6 @@ LOCK TABLES `usuarios` WRITE;
 INSERT INTO `usuarios` VALUES (1,'agostina','cervio','agos@hotmail.com','agosCervio','agos123','2001-08-26'),(2,'santiago','karagozian','santi@hotmail.com','santiKaragozian','santi123','2001-08-26'),(3,'matias','cagliero','mati@hotmail.com','matiCagliero','mati123','2001-08-26'),(4,'ramon','diaz','ramon@hotmail.com','ramonDiaz','ramon','2001-08-26'),(5,'juan','gomez','juan@hotmail.com','juanGomez','juan123','2001-08-26');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
---
--- Table structure for table `productos`
---
-
-DROP TABLE IF EXISTS `productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `productos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(255) NOT NULL,
-  `img_name` varchar(255) DEFAULT,
-  `detalle` varchar(255) NOT NULL,
-  `condicion` varchar(255) NOT NULL,
-  `userAdded` int(11) NOT NULL,
-  `createAt` date NOT NULL,
-  `updatedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_95407dd7-28ee-46dc-92a8-7956bc8217b5` (`user_added`),
-  CONSTRAINT `FK_95407dd7-28ee-46dc-92a8-7956bc8217b5` FOREIGN KEY (`user_added`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `productos`
---
-
-LOCK TABLES `productos` WRITE;
-/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES 
-('1', 'Yerba Mate CHAMIGO 500g', 'yerba.jpg', 'Yerba Chamigo ideal para toda la familia.', '1', '1', '2021-04-25', '2021-04-25')
-('2', 'Papas Noisettes Mc Cain 1kg', 'papas1.jpg', 'Papar ricas para disfrutar.', '4', '2', '2021-04-25', '2021-04-25')
-('3', 'Papas Finas Mc Cain 1kg', 'papas.jpg', 'Papar ricas para disfrutar. Buenisimas!', '4', '2', '2021-04-25', '2021-04-25')
-('4', 'Pan Bimbo Artesano Receta Original 500g', 'pan.jpg', 'Pan de los mejores arrtesanos de Bimbo.', '3', '3', '2021-04-25', '2021-04-25')
-('5', 'Mermelada Patagonia Berries 100% Natural', 'mermelada.jpg', 'Mermelada rica sabor Berries.', '3', '4', '2021-04-25', '2021-04-25')
-('6', 'Liquido limpiador ALA lavado perfecto 3L', 'jabon.jpg', 'Limpiador para ropa ALA.', '4', '4', '2021-04-25', '2021-04-25')
-('7', 'Cerveza Heineken Premium Quality', 'cerveza.jpg', 'Heineken para disfrutar con amigos.', '4', '5', '2021-04-25', '2021-04-25')
-('8', 'Coca Cola Original 1,5L', 'coca_cola.jpg', 'Coca Cola para compartir felicidad.', '4', '6', '2021-04-25', '2021-04-25')
-('9', 'Alfajor Terrabusi Clatico 300g', 'alfajor.jpg', 'Alfabores ricos de chocolate.', '4', '7', '2021-04-25', '2021-04-25')
-('10', 'Fideos Matarazzo 500g', 'fideos.jpg', 'Fideos ricos para compartir en familia. Matarazzo.', '4', '7', '2021-04-25', '2021-04-25')
-
-/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
-UNLOCK TABLES;
---
 
 
 
