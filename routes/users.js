e1b5a3050d42a5659b2e356b926b04e383c2b80a
agosCervio <acervio@udesa.edu.ar>
2021-06-10 16:20:17 -0300

    multer registracion

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const multer = require("multer")
+const path = require("path")
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images/users")
+    },
+    filename: (req, file, cb) => {
+        cb(null, "user-" + Date.now() + path.extname(file.originalname))
+
+    }
+})
+const upload = multer({ storage: storage })
 
 /* GET users listing. */
 
 var controladorUsuario = require("../controllers/controladorUsuario")
 router.get("/registracion", controladorUsuario.registracion)
-router.post("/store", controladorUsuario.store)
+router.post("/store", upload.single("fotoUsuario"), controladorUsuario.store)
 router.get("/login", controladorUsuario.logIn)
 router.post("/login", controladorUsuario.iniciar)
 router.get("/logout", controladorUsuario.logout)
@@ -17,6 +30,10 @@ router.get("/admin/usuarios", controladorUsuario.adminUsuarios)
 router.get('/profile', controladorUsuario.perfil)
 router.get('/profile/edit', controladorUsuario.edit)
 router.post('/profile/edit/:id', controladorUsuario.editado)
+
+
+
+
 
 
 module.exports = router;
