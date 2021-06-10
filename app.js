8bc8a96342f4cad26e0277c76a21588869386c9a
Matias C <matiascagliero1@hotmail.com>
2021-06-10 15:05:58 -0300

    Notas

--- app.js
+++ app.js
@@ -1,51 +1,63 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var app = express();
 var logger = require('morgan');
 
-//requiero session
+// Necesario para Session
+var cookieParser = require('cookie-parser');
 const session = require("express-session")
 
+// Las rutas principales
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/productos');
 
+// Requiero la base de datos
 const db = require("./database/models")
-var app = express();
 
-//configuro session
-app.use(session({
-    secret: "proyecto catalogos",
-    resave: false,
-    saveUninitialized: true
-}))
 
-//info para todas las vistas
-app.use(function(req, res, next) {
+// Configuro session -- Los valores son un estandar. El resave en false y saveUninitialized en true.
+app.use(
+    session({
+        secret: "proyecto_catalogos",
+        resave: false,
+        saveUninitialized: true
+    })
+)
+
+// Comprobar si el el usuario esta logueado en el session
+app.use(function (req, res, next) {
+
     if (req.session.usuarioIngresado != null) {
         res.locals = {
             usuarioLogueado: req.session.usuarioIngresado
         }
-
 
     } else {
         res.locals = {
             usuarioLogueado: null
         }
     }
-    return next()
 
+    return next()
 })
 
-//implementación de cookies
+
+// Implementación de cookies
 app.use(cookieParser())
-    //middleware para ver si el usuario puso recordarme
-app.use(function(req, res, next) {
+
+// middleware para ver si el usuario puso recordarme
+app.use(function (req, res, next) {
+
     if (req.cookies.userId != undefined && req.session.usuarioIngresado === undefined) {
+
         db.Usuario.findByPk(req.cookies.userId)
+
             .then(user => {
+
                 req.session.usuarioIngresado = user
+
                 next()
 
             })
@@ -55,13 +67,16 @@ app.use(function(req, res, next) {
     }
 })
 
-// view engine setup
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -70,17 +85,17 @@ app.use('/users', usersRouter);
 app.use('/productos', productsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
+    // Render the error page
     res.status(err.status || 500);
     res.render('error');
 });
